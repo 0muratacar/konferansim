284ad45ef660908698a8a7b5e4423849bb835c9f
0muratacar <56114760+0muratacar@users.noreply.github.com>
2020-10-01 10:54:37 +0300

    deneme pull

--- AdminPage/tests.py
+++ AdminPage/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import *
+from AdminPage.dbfunctions.MyUserFunc import *
 
 # Create your tests here.
+
+def read_all_users():
+    u = MyUser.objects.all()
+    for us in u:
+        print(us.username)
+        print(us.first_name)
+        print(us.last_name)
+        print(us.email)
+        print(us.password)
+
+read_all_users()
